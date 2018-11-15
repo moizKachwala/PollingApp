a044f901a586c479d8afd03872e9305221141d9a
Moiz.Kachwala <moizk@hotmail.com>
2018-11-15 18:24:34 +0530

    removed run method from start

--- src/main/java/com/example/polls/PollsApplication.java
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -1,9 +1,5 @@
 package com.example.polls;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -17,9 +13,7 @@ import java.util.TimeZone;
 		PollsApplication.class,
 		Jsr310JpaConverters.class
 })
-public class PollsApplication implements ApplicationRunner {
-
-	private static final Logger logger = LogManager.getLogger(PollsApplication.class);
+public class PollsApplication {
 
 	@PostConstruct
 	void init() {
@@ -28,14 +22,5 @@ public class PollsApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollsApplication.class, args);
-	}
-
-	@Override
-	public void run(ApplicationArguments applicationArguments) throws Exception {
-		logger.debug("Debugging log");
-		logger.info("Info log");
-		logger.warn("Hey, This is a warning!");
-		logger.error("Oops! We have an Error. OK");
-		logger.fatal("Damn! Fatal error. Please fix me.");
 	}
 }
